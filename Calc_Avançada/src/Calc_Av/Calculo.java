9412d8a6710afe41bab415155f1b96d7a5d766fb
Carvalho <kfcarvalho.018@gmail.com>
2019-04-26 11:30:16 -0300

    #1 Modificando Exponencial

--- Calc_Avançada/src/Calc_Av/Calculo.java
+++ Calc_Avançada/src/Calc_Av/Calculo.java
@@ -1,41 +1,46 @@
 package Calc_Av;
 public class Calculo {
+    private double result;
+    
     public double Soma(double a, double b){
-        double result = a + b;
-        return result;
+        this.result = a + b;
+        return this.result;
     }
     
     public double Subtracao(double a, double b){
-	double result = a - b;
-        return result;
+	this.result = a - b;
+        return this.result;
     }
 
     public double Multiplicacao(double a, double b){
-	    double result = a * b;
-	    return result;
+	   this.result = a * b;
+            return this.result;
     }
 
     public double Divisao(double a, double b){
-	    double result = a / b;
-	    return result;
+	    this.result = a / b;
+            return this.result;
     }
     
     public double Raiz_Quadrada(double a){
-        double result;
-        result = java.lang.Math.sqrt(a);
-        return result;
+        this.result = java.lang.Math.sqrt(a);
+        return this.result;
     }
 
     public double Porcentagem(double a, double b){
-	    double result;
-	    result = a*b/100;
-	    return result;
+	    this.result = a*b/100;
+            return this.result;
 	}
     public double Logaritmo(double a, double b){
-    	double result;
-	result = Math.log(a)/Math.log(b);
-	return result;
+	this.result = Math.log(a)/Math.log(b);
+	return this.result;
     }
-
+    public double Exponencial(double a, double b){
+        this.result = Math.pow(a, b);
+        return this.result;
+    }
+    public double getResult(){
+        return this.result;
+    }
 	 
 }
--- Calc_Avançada/src/Calc_Av/Principal.java
+++ Calc_Avançada/src/Calc_Av/Principal.java
@@ -5,34 +5,78 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Bem Vindo a Calculadora!");
+        double a = 0, aux = 0;
         Scanner input = new Scanner(System.in);
         Calculo c = new Calculo();
         String equação = input.nextLine();
         String vet[] = equação.split(" ");
-    
-        if((vet[1]).equals("+")){
-            System.out.println(c.Soma(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
-        }else{
-            if((vet[1].equals("-"))){
-                System.out.println(c.Subtracao(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
-            }else{
-                if((vet[1].equals("v"))){
-                    //raiz quadrada
-                    System.out.println(c.Raiz_Quadrada(Double.parseDouble(vet[0])));
-		}else{
-                     if((vet[1].equals("*"))){
-			     System.out.println(c.Multiplicacao(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));		
-                    }else{
-			if((vet[1].equals("%"))){
-				System.out.printls(c.Porcentagem(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
-			}else{
-				if((vet[1].equals("/"))){
-					System.out.printls(c.Divisao(Double.parseDouble(vet[0]), Double.parseDouble(vet[2))));
-				}else{
-					if((vet[1].equals("l"))){
-						System.out.printls(c.Logaritmo(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
-            	    }
-        	}
+        
+        switch (vet[1]){
+            case ("+"):
+                aux = (c.Soma(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
+            break;
+            case ("-"):
+                aux = (c.Subtracao(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
+            break;
+            case ("*"):
+               aux = (c.Multiplicacao(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
+            break;
+            case ("/"):
+                aux = (c.Divisao(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
+            break;
+            case ("v"):
+                aux = (c.Raiz_Quadrada(Double.parseDouble(vet[0])));
+            break;
+            case ("^"):
+                aux = (c.Exponencial(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
+            break;
+            case ("%"):
+                aux = (c.Porcentagem(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
+            break;
+            case("l"):
+                aux = (c.Logaritmo(Double.parseDouble(vet[0]), Double.parseDouble(vet[2])));
+            break;
+            default:
+                System.out.println("erro");
+            break;
+        }
+        System.out.println("= " +aux);
+        do{
+            vet[0] = String.valueOf(c.getResult());
+            vet[1] = input.next(); 
+            vet[2] = input.next(); 
+            switch (vet[1]){
+            case ("+"):
+                aux = (c.Soma(c.getResult(), Double.parseDouble(vet[2])));
+            break;
+            case ("-"):
+                aux = (c.Subtracao(c.getResult(), Double.parseDouble(vet[2])));
+            break;
+            case ("*"):
+               aux = (c.Multiplicacao(c.getResult(), Double.parseDouble(vet[2])));
+            break;
+            case ("/"):
+                aux = (c.Divisao(c.getResult(), Double.parseDouble(vet[2])));
+            break;
+            case ("v"):
+                aux = (c.Raiz_Quadrada(c.getResult()));
+            break;
+            case ("^"):
+                aux = (c.Exponencial(c.getResult(), Double.parseDouble(vet[2])));
+            break;
+            case ("%"):
+                aux = (c.Porcentagem(c.getResult(), Double.parseDouble(vet[2])));
+            break;
+            case("l"):
+                aux = (c.Logaritmo(c.getResult(), Double.parseDouble(vet[2])));
+            break;
+            default:
+                System.out.println("erro");
+            break;
+            }
+        System.out.println("= "+aux);
+        }while (a!=1);
+    }
 }
 
     
